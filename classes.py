584df85b52e530ad7c42fcba00e47dd4545c347a
HopeWiddows <CCC012108@coderacademy.edu.au>
2021-09-17 12:37:51 +1000

    created methods and initialisers

--- classes.py
+++ classes.py
@@ -5,6 +5,9 @@ class Fruit():
     def __init__(self):
         self.flavour, self.colour = random.choice(self.varieties)
     
+    def __repr__(self):
+        return f"<{self.flavour}, {self.colour} {self.__class__.__name__}>"
+    
 class Apple(Fruit):
     varieties = [("sour", "green"), ("sweet", "red")]
 
@@ -17,6 +20,13 @@ class Pear(Fruit):
 class Tree():
     def __init__(self):
         self.fruits = []
+
+    def __repr__(self):
+        return f"{self.fruit_type} tree"
+    
+    def blossom(self):
+        for i in range(self.fecundity):
+            self.fruits.append(self.fruit_type())
 
 class AppleTree(Tree):
    fecundity = 8
@@ -37,4 +47,7 @@ class Cider():
             "sharp": 0
         }
         for fruit in fruitlist:
-            self.flavour[fruit.flavour]+=1
+            self.flavour[fruit.flavour] += 1
+
+def __repr__(self):
+        return f"a barrel of {max(self.flavour, key=lambda key: self.flavour[key])} cider"
